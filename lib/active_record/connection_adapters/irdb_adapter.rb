f8bee7aa56bd16e33353858d749230e8b0c32889
Ray Vernagus <r.vernagus@gmail.com>
2009-05-19 15:33:10 -0400

    Add SqlClientAdapter. Add log statements.

--- lib/active_record/connection_adapters/irdb_adapter.rb
+++ lib/active_record/connection_adapters/irdb_adapter.rb
@@ -11,17 +11,15 @@ module ActiveRecord
       factory       = IRDb::DbProviderFactory.new
       provider      = factory.create_provider provider_name
       db            = IRDb::Database.new(provider, cstr)
-      ConnectionAdapters::IRDbAdapter.new(db, logger)
+      # TODO: detect provider name here and include provider-specific module
+      ConnectionAdapters::SqlClientAdapter.new(db, logger)
     end
   end
   
   module ConnectionAdapters
     class IRDbAdapter < AbstractAdapter
-      def initialize(connection, logger)
-        super
-      end
-      
       def select(sql, name=nil)
+        log sql, name
         begin
           @connection.execute_hash sql
         rescue System::Data::Common::DbException
@@ -30,16 +28,32 @@ module ActiveRecord
       end
       
       def add_limit_offset!(sql, options)
+        log sql, nil
         if options[:limit]
           "SET ROWCOUNT #{options[:limit]}; #{sql}"
         end
       end
       
       def columns(table_name, name=nil)
+        log "Get columns for #{table_name}...", nil
         cols = @connection.columns(table_name)
-        p cols
         cols.map do |col|
           Column.new(col[:name], col[:type], col[:default], col[:nullable?])
+        end
+      end
+    end
+    
+    class SqlClientAdapter < IRDbAdapter
+      def select(sql, name=nil)
+        results = super
+        # TODO: Create result mapping actions that can be used by different providers?
+        results.map do |result|
+          result.each do |k, v|
+            result[k] = v.to_s.rstrip if v.class == System::String
+            result[k] = nil if v.class == System::DBNull
+            p v.class
+          end
+          result
         end
       end
     end
