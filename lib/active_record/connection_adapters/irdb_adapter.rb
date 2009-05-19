13d45b865c9d315bd26d4fc31abd14b710e22a20
Ray Vernagus <r.vernagus@gmail.com>
2009-05-19 10:58:07 -0400

    Enables instance.find(:first).

--- lib/active_record/connection_adapters/irdb_adapter.rb
+++ lib/active_record/connection_adapters/irdb_adapter.rb
@@ -5,8 +5,9 @@ module ActiveRecord
   class Base
     # Establishes a connection to the database that's used by all Active Record objects
     def self.irdb_connection(config) # :nodoc:
+      # TODO: verify provider and connection string
       provider_name = config[:provider]
-      cstr          = config[:connection_string]
+      cstr          = config[:cstr]
       factory       = IRDb::DbProviderFactory.new
       provider      = factory.create_provider provider_name
       db            = IRDb::Database.new(provider, cstr)
@@ -18,6 +19,28 @@ module ActiveRecord
     class IRDbAdapter < AbstractAdapter
       def initialize(connection, logger)
         super
+      end
+      
+      def select(sql, name=nil)
+        begin
+          @connection.execute_hash sql
+        rescue System::Data::Common::DbException
+          raise ActiveRecord::StatementInvalid, "#{$!}"
+        end
+      end
+      
+      def add_limit_offset!(sql, options)
+        if options[:limit]
+          "SET ROWCOUNT #{options[:limit]}; #{sql}"
+        end
+      end
+      
+      def columns(table_name, name=nil)
+        cols = @connection.columns(table_name)
+        p cols
+        cols.map do |col|
+          Column.new(col[:name], col[:type], col[:default], col[:nullable?])
+        end
       end
     end
   end
